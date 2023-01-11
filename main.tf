ed5938d7a0d0490831e8147ebea109b842d5e6a1
openthisworld <puchok80@gmail.com>
2023-01-11 17:38:46 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "sg_website" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami = "ami-0ff8a91507f77f867"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.main_subnet.id
+  security_groups = [aws_security_group.sg_website.name]
+}
